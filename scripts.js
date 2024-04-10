c0e98b6403891af92394675faaf9989b624e99a4
hikh4led <126723224+hikh4led@users.noreply.github.com>
2024-04-09 22:33:13 -0700

    add styling and google font

--- scripts.js
+++ scripts.js
@@ -1,28 +1,3 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- * 
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- * 
- */
-
 const birdData = [
     {
         "name": "House Wren",
@@ -303,7 +278,7 @@ function addBullet(list, label, text) {
 function editCardContent(card, birdName, newImageURL, newHabitat, newFood, newNesting, newBehavior, newConservation) {
     card.style.display = "block";
 
-    const cardHeader = card.querySelector("h2");
+    const cardHeader = card.querySelector(".birdName");
     cardHeader.textContent = birdName;
 
     const cardImage = card.querySelector("img");
@@ -318,6 +293,13 @@ function editCardContent(card, birdName, newImageURL, newHabitat, newFood, newNe
     addBullet(list, "Conservation", newConservation);
 
     console.log("added new bird card:", birdName, "- html: ", card);
+}
+
+function toggleCard(card) {
+    const info = card.querySelector("ul");
+    const img = card.querySelector("img");
+    info.classList.toggle("hidden");
+    img.classList.toggle("hidden");
 }
 
 // This calls the addCards() function when the page is first loaded
